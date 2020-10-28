08f96c370b0641d43f309f8606c914f2256d8e65
al fikri utomo <61920101+alfikri13@users.noreply.github.com>
2020-10-28 12:31:08 +0700

    Create DoorController.cs

---
+++ TheNextCar/Controller/DoorController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheNextCar.Model;
+
+namespace TheNextCar.Controller
+{
+    class DoorController
+    {
+        private Door door;
+        private OnDoorChanged callbackOnDoorChanged;
+
+        public DoorController(OnDoorChanged callbackOnDoorChanged)
+        {
+            this.callbackOnDoorChanged = callbackOnDoorChanged;
+            this.door = new Door();
+        }
+
+        public void close()
+        {
+            this.door.close();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("CLOSED", "door closed");
+        }
+        public void open()
+        {
+            this.door.open();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("OPENED", "door opened");
+        }
+        public void activateLock()
+        {
+            this.door.activatelock();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("LOCKED", "door locked");
+        }
+        public void unlock()
+        {
+            this.door.unlock();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("UNLOCK", "door unlocked");
+        }
+        public bool isClose()
+        {
+            return this.door.isClosed();
+        }
+        public bool isLocked()
+        {
+            return this.door.isLocked();
+        }
+    }
+
+    interface OnDoorChanged
+    {
+        void onlockDoorStateChanged(string value, string message);
+        void onDoorOpenStateChanged(string value, string message);
+    }
+}
